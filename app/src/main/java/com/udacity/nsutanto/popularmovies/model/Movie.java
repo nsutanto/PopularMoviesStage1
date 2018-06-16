927b791873db602b07cb8aaf057203f1c597eb56
Nicholas Sutanto <nsutanto@enova.com>
2018-06-16 09:10:23 -0500

    adding movie title and image url

--- app/src/main/java/com/udacity/nsutanto/popularmovies/model/Movie.java
+++ app/src/main/java/com/udacity/nsutanto/popularmovies/model/Movie.java
@@ -1,5 +1,41 @@
 package com.udacity.nsutanto.popularmovies.model;
 
-public class Movie {
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
+    private String mTitle;
+    private String mImageURL;
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+        public Movie createFromParcel(Parcel in) {
+            return new Movie(in);
+        }
+
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
+
+    public Movie(String title, String imageURL) {
+        mTitle = title;
+        mImageURL = imageURL;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel out, int flags) {
+        out.writeString(mTitle);
+        out.writeString(mImageURL);
+    }
+
+    private Movie(Parcel in) {
+        mTitle = in.readString();
+        mImageURL = in.readString();
+    }
 }
