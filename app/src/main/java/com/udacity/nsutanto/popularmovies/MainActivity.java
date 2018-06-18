abce707ef5b310f33751ea1d69e9432700b0f52c
Nicholas Sutanto <nsutanto@enova.com>
2018-06-18 14:12:35 -0500

    Adding Popular and Top Rated Setting

--- app/src/main/java/com/udacity/nsutanto/popularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/nsutanto/popularmovies/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_popular, menu);
+        getMenuInflater().inflate(R.menu.menu_movie, menu);
         return true;
     }
 
@@ -39,8 +39,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_popular) {
+            // TODO: Fetch Popular Movie
+        }
+        else {
+            // TODO: Fetch Top Rated Movie
         }
 
         return super.onOptionsItemSelected(item);
