c590fbda27c1d9228af8a3f07a5ed456b21b138e
Nicholas Sutanto <nsutanto@enova.com>
2018-06-18 08:32:25 -0500

    Work in progress: Json Utils

--- app/src/main/java/com/udacity/nsutanto/popularmovies/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/nsutanto/popularmovies/utils/JsonUtils.java
@@ -1,6 +1,6 @@
 package com.udacity.nsutanto.popularmovies.utils;
 
-//import com.udacity.nsutanto.model;
+import com.udacity.nsutanto.popularmovies.model.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,36 +11,20 @@ import java.util.List;
 
 public class JsonUtils {
 
-    /*
-    public static final String KEY_NAME = "name";
-    public static final String KEY_MAIN_NAME = "mainName";
-    public static final String KEY_ALSO_KNOW_AS = "alsoKnownAs";
-    public static final String KEY_PLACE_OF_ORIGIN = "placeOfOrigin";
-    public static final String KEY_DESCRIPTION = "description";
-    public static final String KEY_IMAGE = "image";
-    public static final String INGREDIENTS = "ingredients";
 
-    public static Sandwich parseSandwichJson(String json) {
+    public static final String BASE_URL = "https://api.themoviedb.org/3";
+    public static final String API_MOST_POPULAR = "/movie/popular";
+    public static final String API_TOP_RATED = "/movie/top_rated";
 
-        try {
-            JSONObject jsonObj = new JSONObject(json);
-            JSONObject nameObj = jsonObj.getJSONObject(KEY_NAME);
-            String mainName = nameObj.optString(KEY_MAIN_NAME);
-            JSONArray alsoKnownAsArray = nameObj.getJSONArray(KEY_ALSO_KNOW_AS);
-            List<String> alsoKnownAsList = convertToList(alsoKnownAsArray);
 
-            String placeOfOrigin = jsonObj.optString(KEY_PLACE_OF_ORIGIN);
-            String description = jsonObj.optString(KEY_DESCRIPTION);
-            String image = jsonObj.optString(KEY_IMAGE);
-            JSONArray ingredientsArray = jsonObj.getJSONArray(INGREDIENTS);
-            List<String> ingredientList = convertToList(ingredientsArray);
 
-            return new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientList);
-        }
-        catch (JSONException e) {
+    public static List<Movie> ParseMoviesJS(String json) throws JSONException {
 
-        }
-        return null;
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        JSONObject jsonObj = new JSONObject(json);
+
+        return movies;
     }
 
     private static List<String> convertToList(JSONArray jsonArray) throws JSONException {
@@ -51,6 +35,5 @@ public class JsonUtils {
         }
         return strings;
     }
-    */
 }
 
