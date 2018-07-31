3136041e3a72cf040c8e26c53af9be12c5804244
Nicholas Sutanto <nsutanto@enova.com>
2018-07-31 09:28:53 -0500

    Get the Movie Adapter done

--- app/src/main/java/com/udacity/nsutanto/popularmovies/adapter/MovieAdapter.java
+++ app/src/main/java/com/udacity/nsutanto/popularmovies/adapter/MovieAdapter.java
@@ -1,68 +1,69 @@
 package com.udacity.nsutanto.popularmovies.adapter;
 
-import android.app.Activity;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.udacity.nsutanto.popularmovies.R;
+import com.udacity.nsutanto.popularmovies.listener.ITaskListener;
 import com.udacity.nsutanto.popularmovies.model.Movie;
 
-import java.util.List;
+import java.util.ArrayList;
 
-public class MovieAdapter extends ArrayAdapter<Movie> {
-    /**
-     * This is our own custom constructor (it doesn't mirror a superclass constructor).
-     * The context is used to inflate the layout file, and the List is the data we want
-     * to populate into the lists
-     *
-     * @param context        The current context. Used to inflate the layout file.
-     * @param androidFlavors A List of AndroidFlavor objects to display in a list
-     */
-    public MovieAdapter(Activity context, List<Movie> androidFlavors) {
-        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
-        // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
-        super(context, 0, androidFlavors);
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    private ArrayList<Movie> mMovies = new ArrayList<>();
+    private static ITaskListener mTaskListener;
+    private Context context;
+
+    MovieAdapter(ITaskListener taskListener) {
+        mTaskListener = taskListener;
     }
 
-    /**
-     * Provides a view for an AdapterView (ListView, GridView, etc.)
-     *
-     * @param position    The AdapterView position that is requesting a view
-     * @param convertView The recycled view to populate.
-     *                    (search online for "android view recycling" to learn more)
-     * @param parent The parent ViewGroup that is used for inflation.
-     * @return The View for the position in the AdapterView.
-     */
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        // Gets the AndroidFlavor object from the ArrayAdapter at the appropriate position
-        Movie movie = getItem(position);
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        final ImageView mPosterView;
 
-        // Adapters recycle views to AdapterViews.
-        // If this is a new View object we're getting, then inflate the layout.
-        // If not, this view already has the layout inflated from a previous call to getView,
-        // and we modify the View widgets as usual.
-
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.movie_list_item, parent, false);
+        public ViewHolder(View v) {
+            super(v);
+            mPosterView = v.findViewById(R.id.poster_view);
+            v.setOnClickListener(this);
         }
 
-        /*
-        ImageView iconView = (ImageView) convertView.findViewById(R.id.flavor_image);
-        iconView.setImageResource(movie.image);
-
-        TextView versionNameView = (TextView) convertView.findViewById(R.id.flavor_text);
-        versionNameView.setText(movie.versionName
-                + " - " + movie.versionNumber );
-        */
-
-        return convertView;
+        public void onClick(View v) {
+            mTaskListener.StartDetailActivity();
+        }
     }
+
+
+    @Override
+    public MovieAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.movie_list_item, parent, false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String posterPath = mMovies.get(position).GetPosterPath();
+        if (posterPath != "") {
+            // TODO
+            //Picasso.with(context).load("BASE_URL" + "TEST").into(holder.mPosterView);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMovies.size();
+    }
+
+   public void setMovies(ArrayList<Movie> movies) {
+        mMovies = movies;
+        notifyDataSetChanged();
+   }
 }
+
