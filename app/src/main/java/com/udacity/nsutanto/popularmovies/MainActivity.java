511cd7c362731ecdf9a933cb4bcf44315f70a7a1
Nicholas Sutanto <nsutanto@enova.com>
2018-07-31 14:28:22 -0500

    Work in progress

--- app/src/main/java/com/udacity/nsutanto/popularmovies/MainActivity.java
+++ app/src/main/java/com/udacity/nsutanto/popularmovies/MainActivity.java
@@ -1,5 +1,9 @@
 package com.udacity.nsutanto.popularmovies;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,25 +11,36 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 
 import com.udacity.nsutanto.popularmovies.adapter.MovieAdapter;
-import com.udacity.nsutanto.popularmovies.listener.TaskListener;
+import com.udacity.nsutanto.popularmovies.listener.ITaskListener;
+import com.udacity.nsutanto.popularmovies.model.Movie;
 import com.udacity.nsutanto.popularmovies.task.FetchMovieTask;
+import com.udacity.nsutanto.popularmovies.utils.NetworkUtils;
 
-public class MainActivity extends AppCompatActivity implements TaskListener {
+import java.net.URL;
+import java.util.ArrayList;
 
-    private String mSortBy;
+public class MainActivity extends AppCompatActivity implements ITaskListener {
+
     private RecyclerView mRecyclerView;
     private MovieAdapter mMovieAdapter;
+    private ProgressBar mLoadingIndicator;
+    private SortBy mSortBy;
+    private enum SortBy {
+        POPULAR, TOP_RATED
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        init();
+        initUI();
+        initRecylerView();
+
+        loadMovieData();
     }
 
     @Override
@@ -40,69 +55,74 @@ public class MainActivity extends AppCompatActivity implements TaskListener {
         int id = item.getItemId();
 
         if (id == R.id.action_popular) {
-            // TODO: Fetch Popular Movie
+            loadMovieData();
         }
         else {
-            // TODO: Fetch Top Rated Movie
+            loadMovieData();
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public void onPostExecute() {
+    public void OnPostExecute(ArrayList<Movie> movies) {
 
+        //TextView errorMessage =
+        //ProgressBar loadingIndicator
+        //RecylclerView movieGrid
+
+        if (movies.isEmpty()) {
+
+        } else {
+            mMovieAdapter.setMovies(movies);
+        }
+    }
+
+    public URL GetURL() {
+        if (mSortBy == SortBy.TOP_RATED) {
+            return NetworkUtils.BuildTopRatedMovieURL();
+        } else {
+            return NetworkUtils.BuildPopularMovieURL();
+        }
+    }
+
+    public void StartDetailActivity(Movie movie) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("movie", movie);
+        startActivity(intent);
     }
 
     private void loadMovieData() {
-        //FetchMovieTask fetchMovieTask = new FetchMovieTask();
-        //fetchMovieTask.execute();
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+
+        if (info != null && info.isConnectedOrConnecting()) {
+            // Set Loading Indicator
+            // Set Error Message
+            new FetchMovieTask().execute(this);
+        } else {
+            // Provide error message
+            // Set Loading Indicator
+        }
     }
 
-    private void init() {
-        /*
-         * Using findViewById, we get a reference to our RecyclerView from xml. This allows us to
-         * do things like set the adapter of the RecyclerView and toggle the visibility.
-         */
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_movie);
+    private void initUI() {
 
-        // Create layoutManager, a LinearLayoutManager with VERTICAL orientation and shouldReverseLayout == false
-        /*
-         * LinearLayoutManager can support HORIZONTAL or VERTICAL orientations. The reverse layout
-         * parameter is useful mostly for HORIZONTAL layouts that should reverse for right to left
-         * languages.
-         */
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSortBy = SortBy.POPULAR;
+        //mLoadingIndicator = findViewById(R.id.loading_indicator);
+    }
+
+    private void initRecylerView() {
+        mRecyclerView = findViewById(R.id.recyclerview_movie);
+
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        // Set the layoutManager on mRecyclerView
         mRecyclerView.setLayoutManager(layoutManager);
-
-        // Use setHasFixedSize(true) on mRecyclerView to designate that all items in the list will have the same size
-        /*
-         * Use this setting to improve performance if you know that changes in content do not
-         * change the child layout size in the RecyclerView
-         */
         mRecyclerView.setHasFixedSize(true);
+        mMovieAdapter = new MovieAdapter(this);
+        mRecyclerView.setAdapter(mMovieAdapter);
 
-        // Set mForecastAdapter equal to a new ForecastAdapter
-        /*
-         * The ForecastAdapter is responsible for linking our weather data with the Views that
-         * will end up displaying our weather data.
-         */
-        //mMovieAdapter = new MovieAdapter();
-
-        // COMPLETED (44) Use mRecyclerView.setAdapter and pass in mForecastAdapter
-        /* Setting the adapter attaches it to the RecyclerView in our layout. *//*
-        mRecyclerView.setAdapter(mForecastAdapter);
-
-        *//*
-         * The ProgressBar that will indicate to the user that we are loading data. It will be
-         * hidden when no data is loading.
-         *
-         * Please note: This so called "ProgressBar" isn't a bar by default. It is more of a
-         * circle. We didn't make the rules (or the names of Views), we just follow them.
-         *//*
-        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
-
-        */
-        loadMovieData();
     }
 }
